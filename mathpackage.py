3fbc6e2bf744ac808087d58421835d522406abbc
mariamalave-nyc <59543154+mariamalave-nyc@users.noreply.github.com>
2020-01-14 14:02:19 -0500

    mathpackage.py

---
+++ mathpackage.py
@@ -0,0 +1,19 @@
+"""Author: Maria Malave
+Date: 1/14/2020
+"""
+
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2*math.pi*r
+
+# Calculate A
+A = math.pi*r**2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
