cf4ee9328bf5692054df76cd36f9eb8c31672ed5
mariamalave-nyc <59543154+mariamalave-nyc@users.noreply.github.com>
2020-01-20 13:54:15 -0500

    Update JamesBond.py

--- DataCamp/JamesBond.py
+++ DataCamp/JamesBond.py
@@ -1,11 +1,13 @@
 # Author: Maria Malave
-#Date: 01/10/2020
+#Date: 01/10/2020 - updated 01/20/2020
 
-def greet(first_name, last_name):
-    greeting = 'My name is ' + last_name + ', ' + first_name + ' ' + last_name + '!'
+def greet(first_name, last_name, middle_name, age):
+    greeting = 'My name is ' + last_name + ', ' + first_name + ' ' + middle_name +' ' + last_name  + ' and my age is ' + age + '!'
     return greeting
+ 
 
 # Replace with your first and last name.
 # That is, unless your name is already James Bond.
-greet('James', 'Bond')
-print(greet)
+'''greet('James', 'Bond', 'John')
+'''
+print(greet('James', 'Bond', 'John', '50')) 
